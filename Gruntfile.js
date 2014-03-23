0822d1b7be5c721e79ef3a41d003e2db294e099d
W Young <lostnetworking@gmail.com>
2014-03-23 22:19:40 +0100

    fix grunt referneces

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function (grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 		browserify: {
 			dist: {
-				files:{'game.js':['js/wanttoplay.js'] }
+				files:{'js/game.js':['js/index.js'] }
 			},
 			options: {}
 		}
